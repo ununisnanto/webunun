23b8229c93f72905873954366361a0b390d5ff5c
ununisnanto <ununisnanto@gmail.com>
2013-11-12 14:24:38 +0700

    Update product_m.php

--- models/product_m.php
+++ models/product_m.php
@@ -7,12 +7,12 @@ class Product_m extends CI_Model{
 	public function tampil(){ //untuk menampilkan data produk
         $sql="Select * from products ORDER BY ProductID ASC ";
         return $this->db->query($sql)->result();
-    }
+    	}
 	
 	public function tampilcategory(){ //untuk menampilkan  data kategori pada combobox
         $sql="Select * from categories ORDER BY CategoryID ASC ";
         return $this->db->query($sql)->result();
-    }
+    	}
 	
 	function caridata(){ //untuk mencari data produk berdasarkan kategori
 	$c = $this->input->POST('cari');
